9c5b4024cf6e03ee952119992a37497eb2a8503b
David Buchmann <david.buchmann@liip.ch>
2016-03-02 10:33:03 +0100

    adjust request matcher to the symfony model

--- spec/RequestMatcher/RegexRequestMatcherSpec.php
+++ spec/RequestMatcher/RegexRequestMatcherSpec.php
@@ -9,9 +9,9 @@ use PhpSpec\ObjectBehavior;
 
 class RegexRequestMatcherSpec extends ObjectBehavior
 {
-    function let($regex)
+    function let()
     {
-        $this->beConstructedWith($regex);
+        $this->beConstructedWith();
     }
 
     function it_is_initializable()
@@ -24,22 +24,82 @@ class RegexRequestMatcherSpec extends ObjectBehavior
         $this->shouldImplement('Http\Message\RequestMatcher');
     }
 
-    function it_matches_a_request(RequestInterface $request, UriInterface $uri)
+    function it_matches_a_path(RequestInterface $request, UriInterface $uri)
     {
-        $this->beConstructedWith('/test/');
+        $this->beConstructedWith('^/tes?');
 
         $request->getUri()->willReturn($uri);
-        $uri->__toString()->willReturn('/test');
+        $uri->getPath()->willReturn('/test/foo');
 
         $this->matches($request)->shouldReturn(true);
     }
 
-    function it_does_not_match_a_request(RequestInterface $request, UriInterface $uri)
+    function it_does_not_match_a_path(RequestInterface $request, UriInterface $uri)
     {
-        $this->beConstructedWith('/test/');
+        $this->beConstructedWith('#^/tes?#');
 
         $request->getUri()->willReturn($uri);
-        $uri->__toString()->willReturn('/ttttt');
+        $uri->getPath()->willReturn('/ttttt');
+
+        $this->matches($request)->shouldReturn(false);
+    }
+
+
+    function it_matches_a_host(RequestInterface $request, UriInterface $uri)
+    {
+        $this->beConstructedWith(null, 'php-htt?');
+
+        $request->getUri()->willReturn($uri);
+        $uri->getHost()->willReturn('php-http.org');
+
+        $this->matches($request)->shouldReturn(true);
+    }
+
+    function it_does_not_match_a_host(RequestInterface $request, UriInterface $uri)
+    {
+        $this->beConstructedWith(null, 'php-htt?');
+
+        $request->getUri()->willReturn($uri);
+        $uri->getHost()->willReturn('httplug.io');
+
+        $this->matches($request)->shouldReturn(false);
+    }
+
+    function it_matches_a_method(RequestInterface $request)
+    {
+        $this->beConstructedWith(null, null, 'get');
+
+        $request->getMethod()->willReturn('GET');
+
+        $this->matches($request)->shouldReturn(true);
+    }
+
+    function it_does_not_match_a_method(RequestInterface $request)
+    {
+        $this->beConstructedWith(null, null, 'get');
+
+        $request->getMethod()->willReturn('post');
+
+        $this->matches($request)->shouldReturn(false);
+    }
+
+
+    function it_matches_a_scheme(RequestInterface $request, UriInterface $uri)
+    {
+        $this->beConstructedWith(null, null, null, 'http');
+
+        $request->getUri()->willReturn($uri);
+        $uri->getScheme()->willReturn('http');
+
+        $this->matches($request)->shouldReturn(true);
+    }
+
+    function it_does_not_match_a_scheme(RequestInterface $request, UriInterface $uri)
+    {
+        $this->beConstructedWith(null, null, null, 'http');
+
+        $request->getUri()->willReturn($uri);
+        $uri->getScheme()->willReturn('https');
 
         $this->matches($request)->shouldReturn(false);
     }
--- src/RequestMatcher/RegexRequestMatcher.php
+++ src/RequestMatcher/RegexRequestMatcher.php
@@ -6,29 +6,84 @@ use Http\Message\RequestMatcher;
 use Psr\Http\Message\RequestInterface;
 
 /**
- * Match a request with a regex on the uri.
+ * A port of the Symfony RequestMatcher for PSR-7.
  *
+ * @author Fabien Potencier <fabien@symfony.com>
  * @author Joel Wurtz <joel.wurtz@gmail.com>
  */
 final class RegexRequestMatcher implements RequestMatcher
 {
     /**
-     * Matching regex.
-     *
      * @var string
      */
-    private $regex;
+    private $path;
 
-    public function __construct($regex)
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var array
+     */
+    private $methods = array();
+
+    /**
+     * @var string
+     */
+    private $ips = array();
+
+    /**
+     * @var array
+     */
+    private $attributes = array();
+
+    /**
+     * @var string[]
+     */
+    private $schemes = array();
+
+    /**
+     * The regular expressions used for path or host must be specified without delimiter.
+     * You do not need to escape the forward slash / to match it.
+     *
+     * @param string|null          $path       Regular expression for the path
+     * @param string|null          $host       Regular expression for the hostname
+     * @param string|string[]|null $methods    Method or list of methods to match
+     * @param string|string[]|null $schemes    Scheme or list of schemes to match (e.g. http or https)
+     */
+    public function __construct($path = null, $host = null, $methods = [], $schemes = [])
     {
-        $this->regex = $regex;
+        $this->path = $path;
+        $this->host = $host;
+        $this->methods = array_map('strtoupper', (array) $methods);
+        $this->schemes = array_map('strtolower', (array) $schemes);
     }
 
     /**
      * {@inheritdoc}
+     *
+     * @api
      */
     public function matches(RequestInterface $request)
     {
-        return (bool) preg_match($this->regex, (string) $request->getUri());
+        if ($this->schemes && !in_array($request->getUri()->getScheme(), $this->schemes)) {
+            return false;
+        }
+
+        if ($this->methods && !in_array($request->getMethod(), $this->methods)) {
+            return false;
+        }
+
+        if (null !== $this->path && !preg_match('{'.$this->path.'}', rawurldecode($request->getUri()->getPath()))) {
+
+            return false;
+        }
+
+        if (null !== $this->host && !preg_match('{'.$this->host.'}i', $request->getUri()->getHost())) {
+            return false;
+        }
+
+        return true;
     }
 }
