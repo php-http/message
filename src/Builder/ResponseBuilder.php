ae497df6049c6b73204360d7d349ebf8942906ae
Михаил Красильников <m.krasilnikov@yandex.ru>
2016-03-23 11:21:53 +0300

    Single header line parsing extracted to a separate method in ResponseBuilder.

--- src/Builder/ResponseBuilder.php
+++ src/Builder/ResponseBuilder.php
@@ -48,38 +48,13 @@ class ResponseBuilder
      */
     public function setHeadersFromArray(array $headers)
     {
-        $statusLine = trim(array_shift($headers));
-        $parts = explode(' ', $statusLine, 3);
-        if (count($parts) < 2 || substr(strtolower($parts[0]), 0, 5) !== 'http/') {
-            throw new \UnexpectedValueException(
-                sprintf('"%s" is not a valid HTTP status line', $statusLine)
-            );
-        }
-
-        $reasonPhrase = count($parts) > 2 ? $parts[2] : '';
-        $this->response = $this->response
-            ->withStatus((int) $parts[1], $reasonPhrase)
-            ->withProtocolVersion(substr($parts[0], 5));
-
         foreach ($headers as $headerLine) {
             $headerLine = trim($headerLine);
             if ('' === $headerLine) {
                 continue;
             }
 
-            $parts = explode(':', $headerLine, 2);
-            if (count($parts) !== 2) {
-                throw new \UnexpectedValueException(
-                    sprintf('"%s" is not a valid HTTP header line', $headerLine)
-                );
-            }
-            $name = trim(urldecode($parts[0]));
-            $value = trim(urldecode($parts[1]));
-            if ($this->response->hasHeader($name)) {
-                $this->response = $this->response->withAddedHeader($name, $value);
-            } else {
-                $this->response = $this->response->withHeader($name, $value);
-            }
+            $this->addHeader($headerLine);
         }
 
         return $this;
@@ -110,6 +85,49 @@ class ResponseBuilder
         }
 
         $this->setHeadersFromArray(explode("\r\n", $headers));
+
+        return $this;
+    }
+
+    /**
+     * Add header represented by a string.
+     *
+     * @param string $headerLine Response header as a string.
+     *
+     * @return $this
+     *
+     * @throws \UnexpectedValueException For invalid header values.
+     * @throws \InvalidArgumentException For invalid header names or values.
+     */
+    public function addHeader($headerLine)
+    {
+        if (strpos(strtolower($headerLine), 'http/') === 0) {
+            $parts = explode(' ', $headerLine, 3);
+            if (count($parts) < 2) {
+                throw new \UnexpectedValueException(
+                    sprintf('"%s" is not a valid HTTP status line', $headerLine)
+                );
+            }
+
+            $reasonPhrase = count($parts) > 2 ? $parts[2] : '';
+            $this->response = $this->response
+                ->withStatus((int) $parts[1], $reasonPhrase)
+                ->withProtocolVersion(substr($parts[0], 5));
+        } else {
+            $parts = explode(':', $headerLine, 2);
+            if (count($parts) !== 2) {
+                throw new \UnexpectedValueException(
+                    sprintf('"%s" is not a valid HTTP header line', $headerLine)
+                );
+            }
+            $name = trim(urldecode($parts[0]));
+            $value = trim(urldecode($parts[1]));
+            if ($this->response->hasHeader($name)) {
+                $this->response = $this->response->withAddedHeader($name, $value);
+            } else {
+                $this->response = $this->response->withHeader($name, $value);
+            }
+        }
 
         return $this;
     }
